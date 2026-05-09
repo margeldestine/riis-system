4d657417c8c6370567729d973859b3cd3f2c4832
Margel <margeldestinekrizia@gmail.com>
2026-05-10 06:38:20 +0800

    feat: initial spring boot backend setup

---
+++ riis-backend/src/main/java/com/geeks/riis_backend/RiisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.geeks.riis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiisBackendApplication.class, args);
+	}
+
+}
---
+++ riis-backend/src/test/java/com/geeks/riis_backend/RiisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geeks.riis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
